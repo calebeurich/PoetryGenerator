0f28419762e119e53c94bdd149c4c2e07a1633b4
Caleb <calebeurich27@gmail.com>
2021-04-25 19:34:50 -0400

    file being weird
    might have caused a memory leak and am trying to fix

--- executor.py
+++ executor.py
@@ -1,6 +1,29 @@
 import pickle
+import random
 
-seuss_trees = pickle.load( open( 'trees/' + "seuss_trees.pickle", "rb" ) )
-thug_trees = pickle.load( open( 'trees/' + "thug_trees.pickle", "rb" ) )
+with open ('trees/' + 'seuss_trees.pickle', 'rb') as seuss_file:
+    seuss_trees = pickle.load(seuss_file)
+#seuss_trees = pickle.load( open('trees/' + 'seuss_trees.pickle', 'rb'))
+with open ('trees/' + 'thug_trees.pickle', 'rb') as thug_file:
+    thug_trees = pickle.load(thug_file)
+#thug_trees = pickle.load(open('trees/' + 'thug_trees.pickle', 'rb'))
 
-print(thug_trees[300])
+#print(thug_trees[308][0][0])
+
+seuss_tree = random.choice(seuss_trees)
+print(seuss_tree)
+sentence = ''
+for token in (seuss_tree):
+    print(token)
+    print(token[0])
+    if(token[0] in ['.', ',', '!', '?']):
+        try:
+            sentence += token[0]
+        except:
+            sentence += token[0][0]
+    else:
+        try:
+            sentence += ' ' + token[0]
+        except:
+            sentence += ' ' + token[0][0]
+print(sentence)
